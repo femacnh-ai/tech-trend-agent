5b9c739611c63d51f44e1b7ab356e6e3d765aed2
femacnh-ai <femacnh@gmail.com>
2026-03-06 15:11:50 +0900

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,1 +1,19 @@
+import smtplib
+from email.mime.text import MIMEText
+import os
 
+def send_mail(content):
+   sender = os.environ["EMAIL"]
+   password = os.environ["EMAIL_PASSWORD"]
+  
+   msg = MIMEText(content)
+   msg["Subject"] = "Daily Tech Trend"
+   msg["From"] = sender
+   msg["To"] = sender
+  
+   server = smtplib.SMTP_SSL("smtp.gmail.com",465)
+  
+   server.login(sender,password)
+   server.send_message(msg)
+  
+   server.quit()

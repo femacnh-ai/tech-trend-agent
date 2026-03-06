f2d9f12af127e64029a2dce3569763da6ce099ad
femacnh-ai <femacnh@gmail.com>
2026-03-06 15:12:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from search import search_news
+from analyze import analyze
+from mail import send_mail
 
+articles = search_news()
+
+trend = analyze(articles)
+
+send_mail(trend)
+
+print("Trend mail sent")
